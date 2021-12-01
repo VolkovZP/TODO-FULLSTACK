c8d36ae5993329d369cfe906f69939047575a2bc
Volkov <mark.volkovgit@gmail.com>
2021-12-01 11:56:29 +0200

    todosProject

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+
+function App () {
+  return (
+    <div className='App'>
+      <TaskForm />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
